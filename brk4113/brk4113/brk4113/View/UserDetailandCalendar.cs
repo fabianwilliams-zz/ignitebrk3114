dd7f1152ef035a3b78cfbdada0ddf8c4bffbeaf3
Fabian Williams <fabian@adotob.com>
2016-09-08 19:19:46 -0400

    taking a break and commiting code

---
+++ brk4113/brk4113/brk4113/View/UserDetailandCalendar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using brk4113.Model;
+using Xamarin.Forms;
+
+namespace brk4113.View
+{
+    public class UserDetailandCalendar : ContentPage
+    {
+
+        public UserDetailandCalendar(UsersRequest cuRequest )
+        {
+            this.Title = cuRequest.Value[0].DisplayName;
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello" + Title }
+                }
+            };
+        }
+    }
+}
--- brk4113/brk4113/brk4113/View/UserListView.cs
+++ brk4113/brk4113/brk4113/View/UserListView.cs
@@ -1,23 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Reflection.Emit;
 using System.Text;
-
+using brk4113.Model;
+using Newtonsoft.Json;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace brk4113.View
 {
     public class UserListView : ContentPage
     {
+        private ListView listView;
+        public List<UsersRequest> Users { get; private set; }
+
         public UserListView()
         {
+
+            listView = new ListView
+            {
+                
+            };
+
             Content = new StackLayout
             {
+                Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0),
+                Spacing = 3,
+                Orientation = StackOrientation.Vertical,
                 Children = {
-                    new Label { Text = "Placeholder User Page" }
+                    listView
                 }
             };
+
+            listView.ItemSelected += (sender, e) =>
+            {
+                
+                Navigation.PushAsync(new UserDetailandCalendar(e.SelectedItem as UsersRequest));
+            };
         }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", App.AuthenticationResult.AccessToken);
+            var response = await client.GetStringAsync("https://graph.microsoft.com/beta/users");
+            var result = JsonConvert.DeserializeObject<UsersRequest>(response);
+            listView.ItemsSource = result.Value;
+            var cell = new DataTemplate(typeof(TextCell));
+            //this below will use the default cell properties but will customize it later
+            cell.SetBinding(TextCell.TextProperty, "DisplayName");
+            listView.ItemTemplate = cell;
+
+        }
+
+
     }
 }
