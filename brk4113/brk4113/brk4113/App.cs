e9ad51abe85d190b990fb6cd6db5da690dadcc51
Fabian Williams <fabian@adotob.com>
2016-12-07 10:06:21 -0500

    fixed the endpoint to a valid one

--- brk4113/brk4113/brk4113/App.cs
+++ brk4113/brk4113/brk4113/App.cs
@@ -9,9 +9,11 @@ namespace brk4113
 {
     public class App : Application
     {
-        public static string ClientId = "88c9b486-32ea-469d-9104-d43ba85d0ec2";
+        //public static string ClientId = "88c9b486-32ea-469d-9104-d43ba85d0ec2";
+        public static string ClientId = "d87b9be1-a4c8-4777-af9f-922c414f0e3d";
         public static string LoginAuthority = "https://login.microsoftonline.com/fabiansworld.onmicrosoft.com";
-        public static string ReturnUri = "http://ignite-brk3114-redirect";
+        //public static string ReturnUri = "http://ignite-brk3114-redirect";
+        public static string ReturnUri = "http://www.fabiangwilliams.com/brk1123";
         public static string GraphResourceUri = "https://graph.microsoft.com";
         public static AuthenticationResult AuthenticationResult = null;
 
